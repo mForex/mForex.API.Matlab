69964632c0a7ce0992d3c97a91969eb756b508a9
Wojciech Stokowiec <wojciech.stokowiec@hotmail.com>
2014-03-20 20:18:12 +0100

    Project setup

---
+++ mForex.API.Matlab/Events/TickEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using mForex.API;
+using mForex.API.Packets;
+
+namespace mForex.API.Matlab
+{
+    public class TickEventArgs : EventArgs
+    {
+        public Tick[] Ticks { get; private set; }
+    }
+
+    public delegate void TickEventHandler(object sender, TickEventArgs e);
+}
---
+++ mForex.API.Matlab/MatlabClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace mForex.API.Matlab
+{
+    public class MatlabClient
+    {
+        public event TickEventHandler Ticks;
+
+        public MatlabClient()
+        { 
+        
+        }
+
+        protected void OnTicks(TickEventArgs e)
+        {
+            var h = Ticks;
+            if (h != null)
+                EventHandler.RiseSafely(() => h(this, e));
+        }
+    }
+}
